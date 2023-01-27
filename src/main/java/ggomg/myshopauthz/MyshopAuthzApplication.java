e99b10f7df17d1b4525a979a4399c058c025a4a6
wjh2012 <wjh2012@naver.com>
2023-01-27 18:19:18 +0900

    Initial commit

---
+++ src/main/java/ggomg/myshopauthz/MyshopAuthzApplication.java
@@ -0,0 +1,13 @@
+package ggomg.myshopauthz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopAuthzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshopAuthzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ggomg/myshopauthz/MyshopAuthzApplicationTests.java
@@ -0,0 +1,13 @@
+package ggomg.myshopauthz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopAuthzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
