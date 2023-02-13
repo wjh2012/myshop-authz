3e9bed3bdc00d7d140a1f6033c319fe5612af5cd
wjh2012 <wjh2012@naver.com>
2023-02-13 16:32:56 +0900

    feat : user 중복 검증 로직

--- src/main/java/ggomg/myshopauthz/user/UserService.java
+++ src/main/java/ggomg/myshopauthz/user/UserService.java
@@ -10,6 +10,13 @@ public class UserService {
     private final UserRepository userRepository;
 
     public void createUser(Long id, String role){
+        validateDuplicated(id);
         userRepository.save(User.of(id,role));
+    }
+
+    private void validateDuplicated(Long id){
+        if (userRepository.existsById(id)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
