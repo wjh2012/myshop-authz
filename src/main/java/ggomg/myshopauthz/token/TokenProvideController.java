9410d5eb99c43a06f6c2071c6078b11eed6319a5
wjh2012 <wjh2012@naver.com>
2023-02-28 23:45:05 +0900

    refactor: ResponseEntity 적용

--- src/main/java/ggomg/myshopauthz/token/TokenProvideController.java
+++ src/main/java/ggomg/myshopauthz/token/TokenProvideController.java
@@ -1,9 +1,11 @@
 package ggomg.myshopauthz.token;
 
-import ggomg.myshopauthz.token.DTO.AccessRefreshTokenDTO;
-import ggomg.myshopauthz.token.DTO.AccessTokenDTO;
-import ggomg.myshopauthz.token.DTO.TokenRequest;
+import ggomg.myshopauthz.token.DTO.CreateTokenRequest;
+import ggomg.myshopauthz.token.refreshToken.RefreshTokenService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TokenProvideController {
     private final TokenService tokenService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/newToken")
-    public AccessRefreshTokenDTO newToken(@RequestBody TokenRequest request) {
+    public ResponseEntity<AccessRefreshTokenDTO> newToken(@RequestBody CreateTokenRequest request) {
+
         String accessToken = tokenService.provideAccessToken(request.getId());
         String refreshToken = tokenService.provideRefreshToken(request.getId());
 
-        return new AccessRefreshTokenDTO(accessToken, refreshToken);
+        refreshTokenService.saveRefreshToken(request.getId(), refreshToken);
+
+        AccessRefreshTokenDTO response = new AccessRefreshTokenDTO(accessToken, refreshToken);
+        return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/accessToken")
-    public AccessTokenDTO accessToken(@RequestBody TokenRequest request) {
-        return new AccessTokenDTO(tokenService.provideAccessToken(request.getId()));
+    @PostMapping("/refreshToken")
+    public ResponseEntity<AccessRefreshTokenDTO> refreshToken(@RequestBody CreateTokenRequest request) {
+
+        refreshTokenService.findRefreshTokenById(request.getId());
+
+        String accessToken = tokenService.provideAccessToken(request.getId());
+        String refreshToken = tokenService.provideRefreshToken(request.getId());
+
+        AccessRefreshTokenDTO response = new AccessRefreshTokenDTO(accessToken, refreshToken);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class AccessRefreshTokenDTO {
+        private String accessToken;
+        private String refreshToken;
     }
 }
